26f792d247f54804b1ef12a0cbe66dd3a1193bf7
Hoda Salah <37372736+hodasalah@users.noreply.github.com>
2022-04-02 20:25:08 +0300

    add topNav

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -1,5 +1,10 @@
 import React from "react";
 import Logo from "./Logo";
+import {
+	MenuIcon,
+	SearchIcon,
+	ShoppingCartIcon,
+} from "@heroicons/react/outline";
 
 const TopNav = () => {
 	return (
@@ -9,8 +14,30 @@ const TopNav = () => {
 				<Logo />
 			</div>
 			{/* search bar */}
-			<div>
-				<input type="text" name="" id="" />
+			<div className="bg-red-700 hidden sm:flex items-center h-10 rounded-md flex-grow cursor-pointer">
+				<input
+					type="text"
+					className="p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none"
+				/>
+				<SearchIcon className="h-12 p-4 text-white" />
+			</div>
+			{/* right side */}
+			<div className="text-white">
+				{/* 1st block */}
+				<div>
+					<p>Hello Hoda Salah</p>
+					<p>account & Lists</p>
+				</div>
+				{/* 2nd block */}
+				<div>
+					<p>Returns</p>
+					<p>& Orders</p>
+				</div>
+				{/* 3rd block */}
+				<div>
+					<ShoppingCartIcon className="h-10" />
+					Cart
+				</div>
 			</div>
 		</div>
 	);
